d0c4db07bcaf76779882a7708e84f68639b4ef54
Anton Volokonski <anton.volo@gmail.com>
2022-10-12 16:38:42 +0300

    add c# test proj

---
+++ C#/test1/test1/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//FirstApp();
+//static void FirstApp()
+//{
+//    string askName = "Name: ";
+
+//    Console.Write(askName);
+//    string? name = Console.ReadLine();
+//    // int age = Convert.ToInt32(Console.ReadLine());
+//    Console.WriteLine($"Yo, {name}");
+
+//    Console.WriteLine();
+//}
+
+
+//int[] nums = new int[4];
+
+string[] people = { "Tom", "Sam", "Bob" };
+DescTextArray(people);
+
+static void DescTextArray(string[] textArray)
+{
+    for (int i = textArray.Length - 1; i >= 0; i--)
+    {
+        Console.WriteLine(textArray[i]);
+    }
+}
+//Console.WriteLine(people.Length);
+
+
+
+//int aa = 33;
+//int bb = 600;
+//byte cc = ((byte)(aa + bb));
+//Console.WriteLine(cc);
+
+//try
+//{
+//    int a = 33;
+//    int b = 600;
+//    byte c = checked((byte)(a + b));
+//    Console.WriteLine(c);
+//}
+//catch (OverflowException ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+
+//for (int i = 1; i < 10; i++)
+//{
+//    for (int j = 1; j < 10; j++)
+//    {
+//        Console.Write($"{i * j} \t");
+//    }
+//    Console.WriteLine();
+//}
